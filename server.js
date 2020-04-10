6e5905cfd68a4ccb14043ca60da85114ea3fe311
Deep kakadia <kakadiadeepus@gmail.com>
2020-04-10 18:08:11 -0400

    Check

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-const configRoutes = require("./routes");
+const configRoutes = require("./src/routes");
 
 
 app.use(express.json());
 configRoutes(app);
 
-app.listen(3000, () => {
+app.listen(8000, () => {
     console.log("We've now got a server!");
-    console.log("Your routes will be running on http://localhost:3000");
+    console.log("Your routes will be running on http://localhost:8000");
 });
 
 
