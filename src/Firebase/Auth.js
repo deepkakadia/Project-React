a18bff843e9a5928b82d2171b0605234fb898ae9
parthxparab <parthpparab@gmail.com>
2020-04-15 20:30:31 -0400

    navigation

--- src/Firebase/Auth.js
+++ src/Firebase/Auth.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import firebaseApp from './Firebase'
+import React, { useState, useEffect } from "react";
+import firebaseApp from "./Firebase";
 
 export const AuthContext = React.createContext();
 export const AuthProvider = ({ children }) => {
-    const [currentUser, setcurrentUser] = useState(null);
+  const [currentUser, setcurrentUser] = useState(null);
 
-    useEffect(() => {
-        firebaseApp.auth().onAuthStateChanged(setcurrentUser)
-    }, []);
+  useEffect(() => {
+    firebaseApp.auth().onAuthStateChanged(setcurrentUser);
+  }, []);
 
-    return <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>
-
+  return (
+    <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>
+  );
 };
