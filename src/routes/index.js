dbe4b02b5312b9c1032ef4e832e14e6c51f7291e
parthxparab <parthpparab@gmail.com>
2020-04-15 21:18:02 -0400

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,16 @@
 const userRoutes = require("./userRoutes");
+<<<<<<< HEAD
+=======
+const foodRoutes = require("./foodRoutes");
+>>>>>>> parent of a18bff8... navigation
 
 
 const constructorMethod = app => {
     app.use("/api", userRoutes);
+<<<<<<< HEAD
+=======
+    app.use("/api/food", foodRoutes);
+>>>>>>> parent of a18bff8... navigation
     app.use("*", (req, res) => {
         res.status(404).json({ error: "Not found" });
     });
