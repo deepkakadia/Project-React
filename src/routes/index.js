a0af4fa562f498f2dc23e123fbd9c91b14f6ef79
parthxparab <parthpparab@gmail.com>
2020-04-15 21:09:48 -0400

    Revert "Merge pull request #6 from deepkakadia/parth"
    This reverts commit db4318baf0e6bd9824f55932392dfb82d89d1438, reversing
    changes made to ac3d70347eedbeedd42c34744f7ef11b1cff8d51.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,13 @@
 const userRoutes = require("./userRoutes");
 const foodRoutes = require("./foodRoutes");
-const waterRoutes = require("./waterRoutes");
 
-const constructorMethod = (app) => {
-  app.use("/api", userRoutes);
-  app.use("/api/food", foodRoutes);
-  app.use("/api/water", waterRoutes);
 
-  app.use("*", (req, res) => {
-    res.status(404).json({ error: "Not found" });
-  });
+const constructorMethod = app => {
+    app.use("/api", userRoutes);
+    app.use("/api/food", foodRoutes);
+    app.use("*", (req, res) => {
+        res.status(404).json({ error: "Not found" });
+    });
 };
 
 module.exports = constructorMethod;
